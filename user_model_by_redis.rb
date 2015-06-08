de820e76fc2b743dca6b0e579d02793a1bb11cc0
JackDrogon <jack.xsuperman@gmail.com>
2015-06-08 10:53:56 +0800

    Add save operation

--- user_model_by_redis.rb
+++ user_model_by_redis.rb
@@ -7,15 +7,23 @@ class User
   # user:id nickname, email, password
   # email.to.id email => id, 防止重复邮箱注册
   
+  # TODO: cookie 持久化 && 多server共享
+  # FIXME: 在save之前不应该分配id
+  
   attr_reader :id
 
   def initialize(redis_server, new_user = {})
     @redis_server = redis_client
     if new_user[:id]
-      @id = id
+      @id = new_user[:id]
     else
-      @id = @redis_server.incr "users:count"
+      @id = -1
     end
+  end
+
+  def save
+    @id = @redis_server.incr "users:count" if @id == -1
+    @redis_server.hmset("user:#{@id}", {nickname: nickname, email: email, password: password})
   end
 
   def self.auth
